9527f72de39901168f4267a5e71f40ed410b2e40
Alexis Visser <alexis.lvisser@gmail.com>
2026-01-22 21:32:00 -0600

    treesitter had some breaking changes a bit ago, heres a small update for it

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,22 +1,5 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
-	config = function ()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"markdown",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = false },
-			auto_install = true,
-			ignore_install = {}, -- Add any languages that are not wanted to be installed, ever...
-			modules = {}, -- Add any custom modules here.
-		})
-	end
 }
